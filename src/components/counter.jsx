d18926961a4c140c07897015525a27e4917910a1
isaac ikusika <isaacikusika@gmail.com>
2020-10-17 12:20:22 +0100

    updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,28 +3,45 @@ class Counter extends Component {
     state = { 
         count: 1,
         imageUrl: "~/ESS.png",
-        animals: []
+        animals: [
+            "dog", "cat", "goat"
+        ]
      };
 
-     renderAnimals(){
-         if (this.state.animals.length > 0) {
-             return (
-             <ul>
-                {this.state.animals.map(am => <li key={am}>{am}</li>)}
-            </ul>
-            )
-         }
-     }
+     handleIncrement = () => this.setState({count: this.state.count + 1})
+
+     styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+     };
 
     render() { 
 
         return (
         <>
-            {this.state.animals.length === 0 && <p>There are no animals in the freaking zoo!</p>}
-            {this.renderAnimals()}
+            <img src={this.state.imageUrl} alt=""/>
+            <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+            <button 
+            onClick={this.handleIncrement} 
+            style={this.styles} 
+            className="btn btn-secondary btn-sm">
+                Increment
+            </button>
+           
         </>
             
         );
+    }
+
+    getBadgeClass() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 export default Counter;
