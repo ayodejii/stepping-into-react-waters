7bdac143cfa059dcbe00a745ac4a7d509753def0
hizick <isaacikusika@gmail.com>
2020-10-20 04:27:21 +0100

    Revert "Fixed bug - `key` is a reserved list property"

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        id: this.props.id,
+        id: this.props.id, 
         value: this.props.value,
         imageUrl: "~/ESS.png",
         animals: [
             "dog", "cat", "goat"
         ]
-    };
+     };
 
-    styles = {
+     handleIncrement = () => this.setState({value: this.state.value + 1})
+
+     styles = {
         fontSize: 10,
         fontWeight: 'bold'
-    };
+     };
 
-    /** Increment this.state.value by 1 */
-    handleIncrement = () => {
-        const value = this.state.value + 1;
-        this.setState({ value });
-    };
+    render() { 
 
-
-    render() {
         return (
-            <>
-                <img src={this.state.imageUrl} alt="" />
-                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-                <button
-                    onClick={this.handleIncrement}
-                    style={this.styles}
-                    className="btn btn-secondary btn-sm">
-                    Increment
-                </button>
-                <button
-                    onClick={() => {
-                        console.log(this.props);
-                        debugger;
-                    }}
-                    className="btn btn-danger btn-sm m-2">
-                    Delete
-                </button>
-                <br></br>
-            </>
-
+        <>
+            <img src={this.state.imageUrl} alt=""/>
+            <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+            <button 
+            onClick={this.handleIncrement} 
+            style={this.styles} 
+            className="btn btn-secondary btn-sm">
+                Increment
+            </button>
+            <button onClick={() => this.props.onDelete(this.props.id)} 
+            className="btn btn-danger btn-sm m-2">Delete</button><br></br>
+           
+        </>
+            
         );
     }
 
@@ -52,8 +42,8 @@ class Counter extends Component {
         return classes;
     }
 
-    formatCount() {
-        const { value } = this.state;
+    formatCount(){
+        const {value} = this.state;
         return value === 0 ? "Zero" : value;
     }
 }
