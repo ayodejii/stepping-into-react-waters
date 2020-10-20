64c5e0500670459eb6f70eeeda8f230a2f9e2f47
isaac ikusika <isaacikusika@gmail.com>
2020-10-20 16:59:03 +0100

    Updates

---
+++ precache-manifest.63917346e373874009f114763d4e7b5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fef10fe1f46906c2456fdbbd711c351",
+    "url": "/stepping-into-react-waters/index.html"
+  },
+  {
+    "revision": "e16b3d188f816d8d537c",
+    "url": "/stepping-into-react-waters/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "3c58fa41b1e311b8af4f",
+    "url": "/stepping-into-react-waters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e16b3d188f816d8d537c",
+    "url": "/stepping-into-react-waters/static/js/2.3722360a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/stepping-into-react-waters/static/js/2.3722360a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c58fa41b1e311b8af4f",
+    "url": "/stepping-into-react-waters/static/js/main.47e3d299.chunk.js"
+  },
+  {
+    "revision": "8bdf59896409e6f4fde7",
+    "url": "/stepping-into-react-waters/static/js/runtime-main.dd140dcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stepping-into-react-waters/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.08c99c37772197e7ba694c7e73dbcfa9.js"
+  "/stepping-into-react-waters/precache-manifest.63917346e373874009f114763d4e7b5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stepping-into-react-waters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
